3a2d079d89ffe51b888ecd4652548d8400fdc3f8
Caleb Cargill <cargilch@mail.uc.edu>
2022-02-04 13:52:03 -0500

    Update view styles, create login view model

---
+++ src/WhatsTheMove/WhatsTheMove.Core/View Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsTheMove.Core.ViewModels
+{
+    public class LoginViewModel : Common.ViewModelBase
+    {
+
+    }
+}
