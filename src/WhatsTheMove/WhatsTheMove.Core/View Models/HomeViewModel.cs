a1fb61fa9c6e7c812ce1842805c2ffef1ee97387
bruesebt <bruesebt@mail.uc.edu>
2022-03-09 13:28:02 -0500

    Created HomeView

---
+++ src/WhatsTheMove/WhatsTheMove.Core/View Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WhatsTheMove.Core.ViewModels
+{
+    public class HomeViewModel : Common.ViewModelBase
+    {
+
+
+
+
+
+        public HomeViewModel()
+        {
+        }
+    }
+}
