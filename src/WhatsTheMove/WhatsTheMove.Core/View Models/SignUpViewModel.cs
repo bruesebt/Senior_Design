0526bda2cb6eba65cf96d8afe38631b07594d10a
Caleb Cargill <cargilch@mail.uc.edu>
2022-03-09 14:22:33 -0500

    Sign Up View Model Creation

---
+++ src/WhatsTheMove/WhatsTheMove.Core/View Models/SignUpViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsTheMove.Core.ViewModels
+{
+    public class SignUpViewModel : Common.ViewModelBase
+    {
+
+        #region Constructors
+
+        public SignUpViewModel()
+        {
+            User = new UserViewModel()
+            {
+                Username = "Test Username",
+                Password = "Test Password",
+            };
+        }
+
+        #endregion
+
+        #region Properties
+
+        public UserViewModel User
+        {
+            get => _user;
+            set => UpdateOnPropertyChanged(ref _user, value);
+        }
+        private UserViewModel _user = new UserViewModel();
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
--- src/WhatsTheMove/WhatsTheMove/WhatsTheMove/Views/SignUpView.xaml.cs
+++ src/WhatsTheMove/WhatsTheMove/WhatsTheMove/Views/SignUpView.xaml.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using WhatsTheMove.UI.Common;
 
 namespace WhatsTheMove.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SignUpView : ContentPage
+    public partial class SignUpView : ContentPageBase
     {
         public SignUpView()
         {
             InitializeComponent();
+
+            SetContext();
         }
     }
 }
