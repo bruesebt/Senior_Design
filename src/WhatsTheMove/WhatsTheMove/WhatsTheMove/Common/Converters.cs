05abe2bbaf87ecbaa7375117b6d39725e0febffc
Caleb Cargill <cargilch@mail.uc.edu>
2022-03-21 16:53:41 -0400

    Display image in activity card

--- src/WhatsTheMove/WhatsTheMove/WhatsTheMove/Common/Converters.cs
+++ src/WhatsTheMove/WhatsTheMove/WhatsTheMove/Common/Converters.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using WhatsTheMove.Data.Models;
+using System.Linq;
+using System.IO;
 
 namespace WhatsTheMove.UI.Common
 {
@@ -10,6 +13,33 @@ namespace WhatsTheMove.UI.Common
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             return value?.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value?.Equals(true) == true ? parameter : Binding.DoNothing;
+        }
+    }
+
+    public class ByteArrayImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            ImageSource imageSrc = null;
+            if (value is Activity)
+            {
+                Activity activity = (Activity)value;
+                imageSrc = ImageSource.FromUri(new Uri(activity.Icon));
+
+                if (activity.Photos != null && activity.Photos.Any())
+                {
+                    byte[] array = activity.Photos.First().PhotoArray;
+                    var stream = new MemoryStream(array);
+                    imageSrc = ImageSource.FromStream(() => stream);
+                }
+            }            
+
+            return imageSrc;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
